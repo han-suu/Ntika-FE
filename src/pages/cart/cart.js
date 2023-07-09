3f37ea04be973d03e2eae77f0bfaf27934e43022
Hans <reyhan.supangkat01@gmail.com>
2023-07-09 21:26:52 +0700

    +order (WIP)

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -7,16 +7,42 @@ import { useState } from 'react';
 const cookies = new Cookies()
 // import { useState } from 'react';
 function Cart() {
-    // const kebayas = [kebaya1,kebaya2,kebaya3,kebaya4,kebaya5]
+    var someDate = new Date();
+    var numberOfDaysToAdd = 1;
+    var defaultValueStart = new Date(someDate).toISOString().split("T")[0];
+    var date = someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
+    var defaultValueEnd = new Date(date).toISOString().split("T")[0];
     const close = require('../../images/close.png');
 
-    // const Cart = [{"Name":"SAKURA KEBAYA", "Price": 50000, "Qty": 1, "Size": "s"},{"Name":"Kebaya2", "Price": 30000, "Qty": 1, "Size": "m"},{"Name":"Kebaya3", "Price": 90000, "Qty": 1, "Size": "s"},{"Name":"Kebaya4", "Price": 20000, "Qty": 1, "Size": "s"},{"Name":"Kebaya5", "Price": 30000, "Qty": 1, "Size": "xl"}]
-    // const [first, setfirst] = useState(second)
+    
     const [Cart, setCart] = useState([])
     const [User, setUser] = useState()
     const [StartDate, setStartDate] = useState()
     const [EndDate, setEndDate] = useState()
     const [Shipping, setShipping] = useState()
+    const [SubTotal, setSubTotal] = useState(0)
+    const [Total, setTotal] = useState(0)
+    const [ShippingFee, setShippingFee] = useState(0)
+    const [Durasi, setDurasi] = useState(1)
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+    }
+    
+    // let subTotal = 0
+    // let totalPrice = 0
+    // let shippingPrice = 0
+    // let totalPriceString 
+    // const hitungTotal = (durasi)=>{
+    //     if(Shipping==="delivery"){
+    //         shippingPrice = 15000
+    //     }
+    //     Cart.map((item,index)=>{
+    //         subTotal += item.qty*item.price
+    //     })
+    //     subTotal = subTotal * durasi
+    //     totalPrice= subTotal+shippingPrice
+    //     // totalPriceString = numberWithCommas(totalPrice)
+    // }
     let token = cookies.get('user')
         let config = {
               headers:{
@@ -24,6 +50,8 @@ function Cart() {
               }
         };
     useEffect(() => {
+        setStartDate(defaultValueStart)
+        setEndDate(defaultValueEnd)
         let token = cookies.get('user')
         let config = {
               headers:{
@@ -49,23 +77,98 @@ function Cart() {
     }, [])
     
     // 
+    const hitungSub=(x)=>{
+        let subTotal = 0
+        Cart.map((item,index)=>{
+            subTotal += item.qty*item.price
+        })
+        subTotal = subTotal * x
+        setSubTotal(subTotal)
+    }
+    const hitungTotal=()=>{
+        let total_price = SubTotal + ShippingFee
+        setTotal(total_price)
+    }
     const Sewa = ()=>{
         console.log("SEWA")
+        // console.log(Cart)
+        // console.log("Sub Total : ", subTotal)
+        // console.log("shipping Price : ", shippingPrice)
+        // console.log("Total Price : ", totalPrice)
+        if(StartDate===undefined || EndDate===undefined || Shipping===undefined){
+            console.log("ADA FIELD YANG KURANG")
+            return
+        }
+
+        if(EndDate<=StartDate){
+            console.log("Minimal menyewa 1 hari")
+            return
+        }else{
+            console.log("OKAI")
+        }
         let data ={}
-            data.start = StartDate
-            data.end = EndDate
+            
+            data.durasi = Durasi
+            // hitungTotal(diffDays)
+            data.sub_total = SubTotal
+            data.total_price = Total
+            data.shipping_method = Shipping
+            data.shipping_fee = ShippingFee
             data.address = User.Address
-            data.shipping = Shipping
-            console.log(data)
+            data.start_date = StartDate
+            data.end_date = EndDate
+            
+            
+
+        console.log(data)
     }
+    useEffect(() => {
+        const date1 = new Date(StartDate);
+        const date2 = new Date(EndDate);
+        const diffTime = Math.abs(date2 - date1);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+        setDurasi(diffDays)
+    }, [StartDate,EndDate])
+
+    useEffect(() => {
+        hitungSub(Durasi)
+    }, [Durasi])
+    useEffect(() => {
+        hitungTotal()
+    }, [ShippingFee,SubTotal])
+    
     const Ganti = (v)=>{
-        console.log(v)
+        if (v==="delivery") {
+            setShippingFee(15000)
+        }else{
+            setShippingFee(0)
+        }
         setShipping(v)
+    }
+    const GantiDate = (v)=>{
+        console.log("GANTI")
+        if (v<=StartDate){
+            console.log("MASUK")
+            setStartDate(defaultValueStart)
+            setEndDate(defaultValueEnd)
+        }else{
+            setEndDate(v)
+        }
+    }
+    const GantiDateS = (v)=>{
+        console.log("GANTIS")
+        if (v>=EndDate){
+            console.log("MASUKS")
+            setStartDate(defaultValueStart)
+            setEndDate(defaultValueEnd)
+        }else{
+            setStartDate(v)
+        }
     }
     const DeleteItem = (id)=>{
         Axios.delete(`http://127.0.0.1:8080/v1/cart/${id}`, config)
               .then(function (response) {
-                
+
                 Axios.get('http://127.0.0.1:8080/v1/cart', config)
                     .then(function (response) {
                         setCart(response.data.data)
@@ -82,6 +185,7 @@ function Cart() {
     <div className="Cart">
         <Navbar></Navbar>
         <div className="cart-section">
+            {/* <button onClick={()=>{console.log(StartDate); console.log(EndDate);console.log(Durasi);console.log(SubTotal);console.log(Total)}}>LIHAT</button> */}
             <div className="product-list">
                 <p className="section-heading">keranjang mu</p>
                 <div className="cart">
@@ -113,7 +217,7 @@ function Cart() {
             <div className="checkout-section">
                 <div className="checkout-box">
                     <p className="text">Total Belanja, </p>
-                    <h1 className="bill">Rp0</h1>
+                    <h1 className="bill">Rp{Total}</h1>
                     <br/>
                     <select className="shipping-method" defaultValue={"met"} onChange={(e)=>{Ganti(e.target.value)}}>
                         <option value="met" disabled className="option-shipping">Shipping Method</option>
@@ -129,11 +233,11 @@ function Cart() {
                     <form action="">
                         <div className="input-box">
                             <span>mulai sewa</span>
-                            <input type="date" name="" id="" onChange={(e)=>{setStartDate(e.target.value)}}/>
+                            <input type="date" name="" id="" value={StartDate} defaultValue={StartDate} onChange={(e)=>{GantiDateS(e.target.value)}}/>
                         </div>
                         <div className="input-box">
                             <span>akhir sewa</span>
-                            <input type="date" name="" id=""onChange={(e)=>{setEndDate(e.target.value)}}/>
+                            <input type="date" name="" id="" value={EndDate} defaultValue={EndDate} onChange={(e)=>{GantiDate(e.target.value)}}/>
                         </div>
                     </form>
                 </div>
