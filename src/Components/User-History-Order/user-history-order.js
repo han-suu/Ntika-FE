ec256f28d20e7dec66c58ad6808883f9f864bbf0
Hans <reyhan.supangkat01@gmail.com>
2023-07-10 09:13:00 +0700

    +userHistory

---
+++ src/Components/User-History-Order/user-history-order.js
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+function UserHistoryOrder(props) {
+  const logo5 = require('../../images/logo5.png');
+
+  const [Order, setOrder] = useState()
+
+  useEffect(() => {
+    setOrder(props.dataOrder)
+  }, [])
+  
+  return (
+    <div className="user-history-order">
+      <div className="address-section">
+          <div className="user-info">
+              <img src={logo5} className="logo-profile" alt=""/>
+              <p><strong>Tanggal Sewa:</strong> {Order?.start} - {Order?.end} </p>
+              <p><strong>Shipping Method:</strong> {Order?.shipping_method} </p>
+              <p><strong>Total:</strong> {Order?.total}</p>
+              <p><strong>Status:</strong> {Order?.status} </p>
+              
+              <div className="product-container2">
+                {
+                    Order?.items.map((item,index)=>{
+                        return (
+                            <div className="product-card2">
+                                <div className="product-image2">
+                                    <img src={item.image} className="product-thum2" alt=""/>
+                                </div>
+                                <div className="informasi-produk2">
+                                    <h2 className="product-brand2">Ashella Kebaya</h2>
+                                    <p className="product-short-des2"> Size : {item.size}</p>
+                                    <p className="product-short-des2"> jumlah Item : {item.qty}</p>
+                                </div>
+                            </div>
+                        )
+                    })  
+                }
+                
+              </div>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserHistoryOrder;
--- src/pages/userHistory/user-history.js
+++ src/pages/userHistory/user-history.js
@@ -1,54 +1,41 @@
 import './user-history.css';
 import Navbar from '../../Components/navbar/navbar';
+import { useEffect, useState } from 'react';
+import Cookies from 'universal-cookie';
+import Axios from 'axios';
+import UserHistoryOrder from '../../Components/User-History-Order/user-history-order';
+const cookies = new Cookies()
 function UserHistory() {
-  const logo5 = require('../../images/logo5.png');
-  const kebaya1 = require('../../images/kebaya1.jpg');
-  const kebaya2 = require('../../images/kebaya2.jpg');
-  const kebaya4 = require('../../images/kebaya4.jpg');
+  const [Orders, setOrders] = useState([])
+  useEffect(() => {
+    let token = cookies.get('user')
+        let config = {
+              headers:{
+              Authorization: token,
+              }
+        };
+        Axios.get('http://127.0.0.1:8080/v1/user/order', config)
+              .then(function (response) {
+                    setOrders(response.data.data)
+              })
+              .catch(function (error) {
+                    console.log(error);
+              });
+  }, [])
+  
   return (
     <div className="user-history">
       <Navbar></Navbar>
-      <div class="address-section">
-          <div class="user-info">
-              <img src={logo5} class="logo-profile" alt=""/>
-              <p><strong>Tanggal Sewa:</strong> 17/03/2023 - 22/03/2023 </p>
-              <p><strong>Shipping Method:</strong> Diantar </p>
-              <p><strong>Total:</strong> 350.000</p>
-              <p><strong>Status:</strong> Sedang diProses </p>
-              <div class="product-container2">
-                <div class="product-card2">
-                  <div class="product-image2">
-                    <img src={kebaya1} class="product-thum2" alt=""/>
-                  </div>
-                  <div class="informasi-produk2">
-                    <h2 class="product-brand2">Ashella Kebaya</h2>
-                    <p class="product-short-des2"> Size : M</p>
-                    <p class="product-short-des2"> jumlah Item : 3</p>
-                  </div>
-                </div>
-                <div class="product-card2">
-                  <div class="product-image2">
-                    <img src={kebaya2} class="product-thum2" alt=""/>
-                  </div>
-                  <div class="informasi-produk2">
-                    <h2 class="product-brand2">Ashella Kebaya</h2>
-                    <p class="product-short-des2"> Size : M</p>
-                    <p class="product-short-des2"> jumlah Item : 3</p>
-                  </div>
-                </div>
-                <div class="product-card2">
-                  <div class="product-image2">
-                    <img src={kebaya4} class="product-thum2" alt=""/>
-                  </div>
-                  <div class="informasi-produk2">
-                    <h2 class="product-brand2">Ashella Kebaya</h2>
-                    <p class="product-short-des2"> Size : M</p>
-                    <p class="product-short-des2"> jumlah Item : 3</p>
-                  </div>
-                </div>
-              </div>
-          </div>
-          <a href="index.html"><button class="submit-btn">Oke</button></a>
+      <div className="address-section">
+        {
+          Orders.map((item,index)=>{
+              return (
+                <UserHistoryOrder dataOrder = {item}/>
+              )
+          })  
+        }
+          
+          <a href="/"><button className="submit-btn">Oke</button></a>
 
       </div>
     </div>
