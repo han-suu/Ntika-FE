76dbb1012ec8d5f48c12edb7063acaa9d62e549b
Hans <reyhan.supangkat01@gmail.com>
2023-07-12 20:42:02 +0700

    +Recommended (WIP)

--- src/Components/recommended/recommended.js
+++ src/Components/recommended/recommended.js
@@ -1,21 +1,53 @@
+import { useEffect, useState } from 'react';
 import './recommended.css';
-
+import Axios from 'axios';
 function Recommended() {
     const kebaya1 = require('../../images/kebaya1.jpg');
-    const kebaya2 = require('../../images/kebaya2.jpg');
-    const kebaya3 = require('../../images/kebaya3.jpg');
+    // const kebaya2 = require('../../images/kebaya2.jpg');
+    // const kebaya3 = require('../../images/kebaya3.jpg');
     // const kebaya4 = require('../../images/kebaya4.jpg');
     // const kebaya5 = require('../../images/kebaya5.jpg');
     // const kebayas = [kebaya1,kebaya2,kebaya3,kebaya4,kebaya5]
     // const arrow = require('../../images/arrow.png');
 
     // const Products = [{"Name":"Kebaya1", "Price": 50000},{"Name":"Kebaya2", "Price": 30000},{"Name":"Kebaya3", "Price": 90000},{"Name":"Kebaya4", "Price": 20000},{"Name":"Kebaya5", "Price": 30000}]
+
+    const [Content, setContent] = useState([])
+    const [Best, setBest] = useState()
+    const [NewArr, setNewArr] = useState([])
+    useEffect(() => {
+        Axios.get('http://127.0.0.1:8080/v1/best')
+        .then(function (response) {
+            setBest(response.data.best)
+            
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        Axios.get('http://127.0.0.1:8080/v1/newarr')
+        .then(function (response) {
+            setNewArr(response.data.data)
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        Axios.get('http://127.0.0.1:8080/v1/recommended')
+        .then(function (response) {
+            setContent(response.data.data)
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        
+    }, [])
+    
   return (
     <div className="Recommended">
       <section id="collection" className="py-5">
     <div className="container">
         <div className="title">
             <h2 className="position-relative d-inline-block">New Collection</h2>
+            <button onClick={()=>{console.log(Content)}}>LIHAT</button>
         </div>
         <div className="row g-0">
                 <div className="filter-button-group">
@@ -25,19 +57,38 @@ function Recommended() {
                 </div>
 
                 <div className="product-container1">
-                    <div className="product-card best">
-                            <div className="product-image">
-                                <span className="tag">rent</span>
-                                <img src={kebaya1} className="product-thum" alt=""/>
-                                <a href="product.html"><button className="card-btn">add to</button></a>
+                    {
+                    Content?.map((item,index)=>{
+                        console.log(index)
+                        return (
+                            <div className="product-card best">
+                                <div className="product-image">
+                                    <span className="tag">rent</span>
+                                    <img src={item.thumbnail} className="product-thum" alt=""/>
+                                    <a href="product.html"><button className="card-btn">add to</button></a>
+                                </div>
+                                <div className="informasi-produk">
+                                    <h2 className="product-brand">{item.name}</h2>
+                                    <p className="product-short-des"> a short line about kebaya...</p>
+                                    <span className="price">Rp{item.price}</span>
+                                </div>
                             </div>
-                            <div className="informasi-produk">
-                                <h2 className="product-brand">Sakura Kebaya</h2>
-                                <p className="product-short-des"> a short line about kebaya...</p>
-                                <span className="price">Rp50.000</span>
-                            </div>
-                    </div>
-                    <div className="product-card new">
+                        )
+                    })
+                    }
+                </div>
+                
+        </div>
+    </div>
+    <br/>
+</section>
+    </div>
+  );
+}
+
+export default Recommended;
+
+{/* <div className="product-card new">
                             <div className="product-image">
                                 <span className="tag">rent</span>
                                 <img src={kebaya2} className="product-thum" alt=""/>
@@ -62,16 +113,4 @@ function Recommended() {
                                 <span className="price">Rp50.000</span>
                                
                             </div>
-                    </div>
-
-                </div>
-                
-        </div>
-    </div>
-    <br/>
-</section>
-    </div>
-  );
-}
-
-export default Recommended;
+                    </div> */}
